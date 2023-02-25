0b38ddc2ab5c696b0dde99d38049a56cdc790a74
Rehan Arshad <rehanarshad94@yahoo.com>
2023-02-25 11:30:38 -0600

    update

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/internal/operators';
+import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,7 +9,8 @@ const apiUrl = 'https://my-movie-flix.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+// export class UserRegistrationService {
+  export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -45,7 +45,7 @@ registerUser(): void {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      console.log(result);
-     this.snackBar.open(result, 'OK', {
+     this.snackBar.open('user registered successfully', 'OK', {
         duration: 2000
      });
     }, (result) => {
